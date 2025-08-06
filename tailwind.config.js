5d256ba9768bd91b2c6b3d2d8654d354a32502df
emynado <joatradeemy@gmail.com>
2025-08-06 00:43:26 -0400

    feat: add theme toggle and project filtering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        gold: '#FFD700',
+        electric: '#7DF9FF',
+      },
+    },
   },
   plugins: [],
 };
